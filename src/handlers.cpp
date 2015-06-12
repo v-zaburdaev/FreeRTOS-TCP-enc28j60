6f6faf323ca3651bc550e6f3035ea36ad0c9504d
Reikion <Reikion@users.noreply.github.com>
2015-06-12 15:50:09 +0200

    handlers.cpp: remove old interrupts handlers.

--- src/handlers.cpp
+++ src/handlers.cpp
@@ -14,23 +14,9 @@ extern "C" {
          xPortSysTickHandler();
     }
 
-     void EXTI0_IRQHandler(void)
-     {
-          HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_0);
-     }
-
-     void HAL_GPIO_EXTI_Callback(uint16_t pin)
-     {
-          EXTI0_IRQHandlerCpp();
-     }
-}
-
-void EXTI0_IRQHandlerCpp()
-{
-    static uint32_t tickstart = 0;
-    if((HAL_GetTick() - tickstart) > 50) {
-        click_counter++;
-        tickstart = HAL_GetTick();
+    void HAL_GPIO_EXTI_Callback(uint16_t pin)
+    {
+        EXTI0_IRQHandlerCpp();
     }
-
 }
+
