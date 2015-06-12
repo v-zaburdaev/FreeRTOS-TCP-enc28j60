2c7863b571f830275daf2e43556791568b6070ad
Reikion <Reikion@users.noreply.github.com>
2015-06-12 15:37:39 +0200

    FreeRTOS: Increase heap size. Enable timers.

--- include/FreeRTOSConfig.h
+++ include/FreeRTOSConfig.h
@@ -96,7 +96,7 @@
 #define configTICK_RATE_HZ				( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES			( 5 )
 #define configMINIMAL_STACK_SIZE		( ( unsigned short ) 130 )
-#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 75 * 1024 ) )
+#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 100 * 1024 ) )
 #define configMAX_TASK_NAME_LEN			( 10 )
 #define configUSE_TRACE_FACILITY		1
 #define configUSE_16_BIT_TICKS			0
@@ -115,7 +115,7 @@
 #define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
 
 /* Software timer definitions. */
-#define configUSE_TIMERS				0
+#define configUSE_TIMERS				1
 #define configTIMER_TASK_PRIORITY		( 2 )
 #define configTIMER_QUEUE_LENGTH		10
 #define configTIMER_TASK_STACK_DEPTH	( configMINIMAL_STACK_SIZE * 2 )
@@ -129,6 +129,7 @@ to exclude the API function. */
 #define INCLUDE_vTaskSuspend			1
 #define INCLUDE_vTaskDelayUntil			1
 #define INCLUDE_vTaskDelay				1
+#define INCLUDE_xTimerPendFunctionCall  1
 
 /* Cortex-M specific definitions. */
 #ifdef __NVIC_PRIO_BITS
